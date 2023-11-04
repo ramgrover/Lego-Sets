3d015b4471aba6f80e2d5dcdf01ee53be802e363
Ram Grover <ramgrover.rg@gmail.com>
2023-11-03 22:27:31 -0400

    initial commit

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ const legoData = require("./modules/legoSets");
 const express = require("express");
 const path = require("path");
 const app = express();
+app.set('view engine', 'ejs');
 const HTTP_PORT = process.env.PORT || 8080;
 
 app.use(express.static("public"));
@@ -22,34 +23,62 @@ app.use(express.static("public"));
 
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname,"/views/home.html"));
+   /**  res.sendFile(path.join(__dirname,"/views/home.html"));*/
+    res.render("home");
   });
 
   app.get("/about",(req,res)=>{
-    res.sendFile(path.join(__dirname,"/views/about.html"));
+    /**res.sendFile(path.join(__dirname,"/views/about.html"));*/
+    res.render("about");
   });
   
-  app.get("/lego/sets", (req, res) => {
-    if(req.query.theme){
-      legoData.getSetsByTheme(req.query.theme)
-      .then((data)=>res.status(200).json(data))
-      .catch((err)=>res.status(404).sendFile(path.join(__dirname,"/views/404.html"))
-      );
+  //  app.get("/lego/sets", (req, res) => {
+  //   if(req.query.theme){
+  //     legoData.getSetsByTheme(req.query.theme)
+  //     .then((data)=>res.status(200).render("sets",{sets:data}))
+  //     .catch((err)=>res.status(404).render("404")/** .sendFile(path.join(__dirname,"/views/404.html"))*/
+  //     );
 
-    }
+  //   } 
 
-    legoData.getAllSets().then((data)=>res.json(data))
-    .catch((err)=>
-    res.status(404).sendFile(path.join(__dirname,"/views/404.html"))
-    );
-  });
+    app.get("/lego/sets", (req, res) => {
+
+      if (req.query.theme) {
+    
+        legoData.getSetsByTheme(req.query.theme)
+    
+          .then((data) => res.status(200).render("sets", { sets: data }))
+    
+          .catch((err) => res.status(404).render("404", {message: "I'm sorry, no sets were found for the specified theme!"})/** .sendFile(path.join(__dirname,"/views/404.html"))*/
+    
+          );
+    
+      } else {
+    
+        legoData.getAllSets().then((data) => res.render("sets", { sets: data }))
+    
+          .catch((err) =>
+    
+            res.status(404).render("404")//sendFile(path.join(__dirname,"/views/404.html"))
+    
+          );
+    
+      }
+    
+    });
+
+    // legoData.getAllSets().then((data)=>res.render("sets",{sets:data}))
+    // .catch((err)=>
+    // res.status(404).render("404")//sendFile(path.join(__dirname,"/views/404.html"))
+    // );
+  
   
   app.get("/lego/sets/:id", (req, res) => {
     legoData
       .getSetByNum(req.params.id)
-      .then((data) => res.json(data))
+      .then((data) => res.render("set",{set:data}))
       .catch((err) =>
-        res.status(404).sendFile(path.join(__dirname, "/views/404.html"))
+        res.status(404).render("404", {message: "I'm sorry, the specified set was not found!"})//sendFile(path.join(__dirname, "/views/404.html"))
       );
   });
   
@@ -61,7 +90,7 @@ app.get("/", (req, res) => {
       .catch(error => {res.json(error)});
   });*/
   app.use((req,res)=>{
-    res.status(404).sendFile(path.join(__dirname,"/views/404.html"));
+    res.status(404).render("404", {message: "I'm sorry, we're unable to find what you're looking for"})//sendFile(path.join(__dirname,"/views/404.html"));
   });
 
 
